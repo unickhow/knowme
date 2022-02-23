4980e27d2134fa115d80b289899b9a78ec4cefb7
unick_how <unick.zhow@gmail.com>
2022-02-24 01:43:38 +0800

    🧬 refactor: data group

--- src/App.vue
+++ src/App.vue
@@ -7,4 +7,9 @@ import CardGenerator from './components/cardGenerator.vue';
 </script>
 
 <style>
+html, body {
+  background-color: #ececec;
+  margin: 0;
+  padding: 0;
+}
 </style>
--- src/components/cardGenerator.vue
+++ src/components/cardGenerator.vue
@@ -2,70 +2,101 @@
   <main id="card-generator">
     <div class="container max-w-[800px] min-h-100vh mx-auto shadow-xl px-6 py-4">
       <h1 class="mb-2">Hail Anurag !</h1>
-      <p class="mt-0 mb-8">inspired by <a href="https://github.com/anuraghazra/github-readme-stats" target="_blank">github-readme-stats</a></p>
+      <p class="mt-0 mb-8">Inspired by <a href="https://github.com/anuraghazra/github-readme-stats" target="_blank">github-readme-stats</a></p>
 
-      <div class="flex flex-col mb-4">
-        <legend class="mb-2 font-bold">GitHub's username</legend>
-        <input type="text" v-model="form.username">
-      </div>
-      <div class="flex flex-col mb-4">
-        <legend class="mb-2 font-bold">Hiding Stats</legend>
-        <div v-for="stat in Stats" :key="stat">
-          <input :id="`stat_${stat}`" type="checkbox" name="hiding-stats" v-model="form.hide" :value="stat">
-          <label :for="`stat_${stat}`">{{ stat }}</label>
+      <div class="field-group field--username">
+        <legend class="mb-4 -ml-3 font-bold flex text-xl tracking-wide">
+          <mdiDrawPen class="mr-2 text-[#F19F19]" />
+          GitHub's username
+        </legend>
+        <div class="flex items-center text-gray-600">
+          <span class="mr-1">@</span>
+          <input type="text" autofocus v-model.trim="username" class="is-input w-[150px]" @keyup.enter="handleGenerate">
         </div>
       </div>
-      <div class="flex flex-col mb-4">
-        <legend class="mb-2 font-bold">Others</legend>
-        <div>
-          <input id="count_private" type="checkbox" v-model="form.count_private" :value="true">
-          <label for="count_private">count_private</label>
+      <div class="field-group field--hiding-stats">
+        <legend class="mb-4 -ml-3 font-bold flex text-xl tracking-wide">
+          <mdiDrawPen class="mr-2 text-[#F19F19]" />
+          Hiding Stats
+        </legend>
+        <div v-for="(value, key) in Stats" :key="key" class="mb-2">
+          <label class="cursor-pointer text-gray-600">
+            <input
+              type="checkbox"
+              name="hiding-stats"
+              class="mr-2"
+              v-model="hidingStats"
+              :value="key">
+            <span>{{ value }}</span>
+          </label>
         </div>
-        <div>
-          <input id="show_icons" type="checkbox" v-model="form.show_icons" :value="true">
-          <label for="show_icons">show_icons</label>
+      </div>
+      <div class="field-group field--others">
+        <legend class="mb-4 -ml-3 font-bold flex text-xl tracking-wide">
+          <mdiDrawPen class="mr-2 text-[#F19F19]" />
+          Others
+        </legend>
+        <div v-for="(value, key) in boolFlags" :key="key" class="mb-2">
+          <label class="cursor-pointer text-gray-600">
+            <input
+              type="checkbox"
+              class="mr-2"
+              v-model="boolFlags[key]"
+              :value="value">
+            <span>{{ key }}</span>
+          </label>
         </div>
       </div>
 
-      <p class="bg-gray-100 rounded p-4">Preview:<br>
+      <div class="bg-gray-100 rounded p-4 mb-4 break-all">
+        <legend class="mb-2 font-bold">Preview: </legend>
         <code>{{ assembledApi }}</code>
-      </p>
+      </div>
 
-      <button @click="handleGenerate">Generate</button>
-      <div class="mt-8">
-        <img :src="imgSrc" alt="">
+      <button class="is-btn flex items-center mx-auto" :disabled="!username" @click="handleGenerate">
+        <mdiCardAccountDetailsStarOutline class="mr-2" />
+        <span>Generate</span>
+      </button>
+      <div class="my-8 text-center">
+        <img class="max-w-full" :src="imgSrc" alt="">
       </div>
     </div>
   </main>
 </template>
 
 <script lang="ts" setup>
+import mdiCardAccountDetailsStarOutline from '~icons/mdi/card-account-details-star-outline';
+import mdiDrawPen from '~icons/mdi/draw-pen';
+import { useThrottleFn } from '@vueuse/core'
+
+const username = ref('')
+
 enum Stats {
   stars = 'stars',
   commits = 'commits',
-  prs = 'prs',
+  prs = 'pull requests',
   issues = 'issues',
-  contribs = 'contribs'
+  contribs = 'contributes'
 }
+const hidingStats = ref([])
 
-const isGenerate = ref(false)
-const imgSrc = ref('')
-const form = reactive({
-  username: '',
-  hide: [],
+const boolFlags = ref({
   count_private: false,
-  show_icons: false
+  show_icons: false,
+  hide_title: false,
+  hide_rank: false,
+  include_all_commits: false,
+  disable_animations: false
 })
 
 const allQueries:string = computed(() => {
-  let str: string[] = []
-  for (const [key, value] of Object.entries(form)) {
-    if (typeof value === 'string') {
-      str.push(`${key}=${value}`)
-    } else if (Array.isArray(value)) {
-      str.push(`${key}=${value.join(',')}`)
-    } else if (typeof value === 'boolean' && !!value) {
-      str.push(`${key}=${value}`)
+  let str: string[] = [`username=${username.value}`]
+  if (hidingStats.value.length) {
+    str.push(`hide=${hidingStats.value.join(',')}`)
+  }
+  for (const [key, value] of Object.entries(boolFlags.value)) {
+    if (value) {
+      str.push(`${key}=true`)
     }
   }
   return str.join('&')
@@ -73,7 +104,30 @@ const allQueries:string = computed(() => {
 // [![${form.username}'s GitHub stats](https://github-readme-stats.vercel.app/api?${allQueries.value})](https://github.com/anuraghazra/github-readme-stats)
 const assembledApi = computed(() => `https://github-readme-stats.vercel.app/api?${allQueries.value}`)
 
-const handleGenerate = () => {
+const imgSrc = ref('')
+const handleGenerate = useThrottleFn(() => {
+  if (!username.value) return false
   imgSrc.value = assembledApi.value
-}
+}, 1000, false)
 </script>
+
+<style scoped>
+.container {
+  background-color: #fff;
+}
+.field-group {
+  @apply flex flex-col mb-4 rounded-md p-4;
+  box-shadow: 10px 10px 15px rgba(225, 225, 225, 0.5);
+}
+
+.is-btn {
+  @apply text-gray-700 bg-transparent px-4 py-2 border-none rounded-full cursor-pointer transition-colors transition-shadow;
+  @apply hover:text-gray-900 active:translate-y-0.2 active:shadow;
+  @apply disabled:shadow-none disabled:text-gray-400;
+  box-shadow: -10px -10px 15px rgba(245, 245, 245, .5), 6px 6px 15px rgba(70, 70, 70, .12);
+}
+
+.is-btn:active svg {
+  color: #F19F19;
+}
+</style>
